de2474d6a044edd1fbc281960a66a7cc06f137ee
jayPreak <jayesh.preak2003@gmail.com>
2022-12-03 16:55:06 +0530

    cdcvf

--- server/index.js
+++ server/index.js
@@ -6,14 +6,8 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
-const corsOptions ={
-  origin: process.env.ORIGIN, 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions))
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 mongoose
